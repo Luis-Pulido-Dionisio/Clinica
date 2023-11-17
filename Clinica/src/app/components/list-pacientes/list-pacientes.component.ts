b3bfe3e0a51f81a32157d6fccc2b039c7c56690b
Luis-Pulido-Dionisio <116675047+Luis-Pulido-Dionisio@users.noreply.github.com>
2023-11-16 20:10:12 -0600

    Modificaciones
    Aver si jala xd

--- Clinica/src/app/components/list-pacientes/list-pacientes.component.ts
+++ Clinica/src/app/components/list-pacientes/list-pacientes.component.ts
@@ -6,12 +6,12 @@ import { Paciente } from 'src/app/interfaces/Paciente';
 
 
 const listPaciente : Paciente[] = [
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date},
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date},
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date},
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date},
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date},
-  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "", Genero: "", Telefono: "", Fecha_Nac :new Date}
+  {Nombre_Pac: "Luis", Apellido_Pac : "Pulido", Direccion : "Guillermo Hernandez", Genero: "Masculino", Telefono: "274-175-5442", Fecha_Nac :new Date},
+  {Nombre_Pac: "Shirel", Apellido_Pac : "Vazquez", Direccion : "", Genero: "", Telefono: "Femenino", Fecha_Nac :new Date},
+  {Nombre_Pac: "Deisy", Apellido_Pac : "Castro", Direccion : "", Genero: "Femenino", Telefono: "", Fecha_Nac :new Date},
+  {Nombre_Pac: "Kimberley", Apellido_Pac : "Perez", Direccion : "", Genero: "Femenino", Telefono: "", Fecha_Nac :new Date},
+  {Nombre_Pac: "Keila", Apellido_Pac : "XD", Direccion : "", Genero: "Femenino", Telefono: "", Fecha_Nac :new Date},
+  {Nombre_Pac: "XD", Apellido_Pac : "XD", Direccion : "", Genero: "Femenino", Telefono: "", Fecha_Nac :new Date}
 ];
 
 @Component({
@@ -21,7 +21,7 @@ const listPaciente : Paciente[] = [
 })
 export class ListPacientesComponent implements AfterViewInit {
 
-  displayedColumns: string[] = ['Nombre_Pac', 'Apellido_Pac', 'Direccion', 'Genero', 'Fecha_Nac'];
+  displayedColumns: string[] = ['Nombre_Pac', 'Apellido_Pac', 'Direccion', 'Genero', 'Fecha_Nac', 'Acciones'];
   dataSource: MatTableDataSource<Paciente>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -36,5 +36,14 @@ export class ListPacientesComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
    // this.dataSource.paginator._intl.itemsPerPageLabel = "item por pagina"
    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
